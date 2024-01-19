2c5eeea2dc4916343edfac18a1360e97e9b0bcd6
Bram Wiggers <orangenodes@gmail.com>
2024-01-19 09:42:45 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Introduction
+
+Welcome to this beginner repo. 
+It is used to learn the basics of git and github.
---
+++ starting_pandas.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Create a DataFrame from a dictionary
+data = {'name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
+        'year': [2012, 2012, 2013, 2014, 2014],
+        'reports': [4, 24, 31, 2, 3]}
+df = pd.DataFrame(data, index=['Cochice', 'Pima', 'Santa Cruz', 'Maricopa', 'Yuma'])
+
+# View the top 3 rows
+print(df.head(3))
